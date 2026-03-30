d5d6a89215de6aaedb977dcb0ae2d91aa28c594a
Luiz Fernando <Luiz Fernando>
2026-03-30 19:27:33 -0300

    Usuário module

--- src/usuario/usuario.repository.ts
+++ src/usuario/usuario.repository.ts
@@ -1,10 +1,12 @@
+import { Injectable } from "@nestjs/common";
+
+@Injectable()
 export class UsuarioRepository{
 
     private usuarios: Usuario[] = [];
 
     async salvar(usuario){
         this.usuarios.push(usuario);
-        //console.log(this.usuarios);
     }
 
         async listar(){
