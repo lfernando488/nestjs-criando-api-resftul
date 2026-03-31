c0dc571715d72987a8007e1ae78f66dc1434b9c6
Luiz Fernando <Luiz Fernando>
2026-03-31 12:41:36 -0300

    Usuário entity

--- src/usuario/usuario.controller.ts
+++ src/usuario/usuario.controller.ts
@@ -1,6 +1,8 @@
 import { CriaUsuarioDto } from './dto/CriaUsuario.Dto';
+import { UsuarioEntity } from './usuario.entity';
 import { UsuarioRepository } from './usuario.repository';
 import { Body, Controller, Get, Post } from '@nestjs/common';
+import { v4 as uudi} from 'uuid';
 
 @Controller('/usuarios')
 export class UsuarioController {
@@ -11,8 +13,15 @@ export class UsuarioController {
 
     @Post()
     async criaUsuario(@Body() dadosDoUsuario: CriaUsuarioDto) {
-        this.usuarioRepository.salvar(dadosDoUsuario);
-        return dadosDoUsuario;
+        
+        const usuarioEntity = new UsuarioEntity();
+        usuarioEntity.email = dadosDoUsuario.email;
+        usuarioEntity.senha = dadosDoUsuario.senha;
+        usuarioEntity.nome = dadosDoUsuario.nome;
+        usuarioEntity.id = uudi();
+
+        this.usuarioRepository.salvar(usuarioEntity);
+        return {id: usuarioEntity.id, message: 'Usuário criado com sucesso!' };
     }
 
     @Get()
